70886795a1e120a4e829acaec2ab86843a7c6d6f
Pranil Rathod <rathod1337@gmail.com>
2021-01-18 04:39:04 +0530

    feat: Add vuex and vuetify

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+	modules: {},
+	plugins: [createPersistedState()],
+})
+
+export default store
